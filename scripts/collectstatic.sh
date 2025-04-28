89f45e6ce5720c790ebcedb2b0ec0137ae8bf09c
Pedro Iegler <produto@conor.com.br>
2025-04-28 14:23:55 -0300

    feat: scripts

--- scripts/collectstatic.sh
+++ scripts/collectstatic.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-python manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
